71fd0a3be0e644bdbbd40a076628321f930c5584
Bfoody <bcfoody@gmail.com>
2018-09-06 10:36:01 -0700

    comment test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//test comment
+//test comment 1
 public class Main extends Application {
 
     @Override
