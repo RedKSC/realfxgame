9bd2eefb3658337d6c50c2bdea33b8fb8727d74f
toppergalen <galen.topper21@bcp.org>
2018-09-06 10:56:17 -0700

    test commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,13 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    //YEET
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("All bow down for...");
+        primaryStage.setTitle("Hola como esta");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        System.out.print("Galen, king of memes");
+        System.out.print("YEEEEEEEEEEET");
     }
 
 
