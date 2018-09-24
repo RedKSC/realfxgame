30bead855fae009ec9131df9d3ed2917c969697f
Nolan Topper <nolan.topper@gmail.com>
2018-09-24 13:35:47 -0700

    stackint

--- src/sample/StackInt.java
+++ src/sample/StackInt.java
@@ -1,8 +1,25 @@
 package sample;
-
 public class StackInt {
+    int[] stack;
+    int stackPointer;
+    int max;
     StackInt(int i) {
         System.out.printf("Creating Object %d%n", i);
+        stack = new int[i];
+        stackPointer = 0;
+        max = i;
         return;
+    }
+    int pop() throws stackException {
+        if (stackPointer == 0) {
+            throw (new stackException() );
+        }
+        return (stack[--stackPointer]);
+    }
+    void push(int i) throws stackException {
+        if (stackPointer == max ) {
+            throw ( new stackException() );
+        }
+        stack[stackPointer++] = i;
     }
 }
