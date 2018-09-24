38d11f00b1bcc9f7472f18d7ff17d07c9b2d7467
Nolan Topper <nolan.topper@gmail.com>
2018-09-24 13:42:20 -0700

    stackint

--- src/sample/StackInt.java
+++ src/sample/StackInt.java
@@ -1,116 +1,45 @@
 package sample;
 public class StackInt {
-
-
-
-    int [] stack;
-
+    int[] stack;
     int stackPointer;
-
     int max;
-
-
-
     StackInt(int i) {
-
         System.out.printf("Creating Object %d%n", i);
-
-
-
         stack = new int[i];
-
         stackPointer = 0;
-
         max = i;
-
-
-
         return;
-
     }
-
-
-
     int pop() throws stackException {
-
         if (stackPointer == 0) {
-
             throw (new stackException() );
-
         }
-
         return (stack[--stackPointer]);
-
     }
-
-
-
     void push(int i) throws stackException {
-
         if (stackPointer == max ) {
-
             throw ( new stackException() );
-
         }
-
         stack[stackPointer++] = i;
-
     }
-
-
     public static class stackIntTest {
-
-
-
         public static void main(String[] args) {
-
-
-
             StackInt s = new StackInt(3);
-
-
-
             int i;
-
-
-
             for ( i=1; i < 5; i++) {
-
                 try {
-
                     s.push(i);
-
-
-
                 } catch (stackException e) {
-
                     System.out.println("pushed to much");
-
                 }
-
             }
-
-
-
             for ( i=0; i < 5; i++) {
-
                 try {
-
                     System.out.printf("%d%n",  s.pop());
-
                 } catch (stackException e) {
-
                     System.out.println("popped to much");
-
                 }
-
             }
-
-
-
-
-
         }
-
     }
 }
--- src/sample/StackIntTest.java
+++ src/sample/StackIntTest.java
@@ -1,58 +1,21 @@
 package sample;
-
-
-public class stackIntTest {
-
-
-
+public class StackIntTest {
     public static void main(String[] args) {
-
-
-
         StackInt s = new StackInt(3);
-
-
-
         int i;
-
-
-
         for ( i=1; i < 5; i++) {
-
             try {
-
                 s.push(i);
-
-
-
             } catch (stackException e) {
-
                 System.out.println("pushed to much");
-
             }
-
         }
-
-
-
         for ( i=0; i < 5; i++) {
-
             try {
-
                 System.out.printf("%d%n",  s.pop());
-
             } catch (stackException e) {
-
                 System.out.println("popped to much");
-
             }
-
         }
-
-
-
-
-
     }
-
 }
